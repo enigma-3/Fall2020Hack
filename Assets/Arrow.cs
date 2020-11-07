fce884f87489647bb9c74ae57295dfb1f79ddcf4
Kenneth <jll26@sfu.ca>
2020-11-07 14:15:32 -0800

    arrow destroyed at timer

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -6,11 +6,15 @@ public class Arrow : MonoBehaviour
 {
     Rigidbody2D rb;
     bool hasHit;
-    
+    float startTime;
+    float duration = 4.0f;
+    public GameObject arrow;
 
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
+
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -20,7 +24,12 @@ public class Arrow : MonoBehaviour
    
         float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-       
+
+        if(Time.time - startTime > duration){
+            Destroy(this.gameObject);
+        }
+
+    
     }
 
     // private void OnCollisionEnter2D(Collision2D colision){
